a4fff0c30e991ef5a2d2ab02e6d425b7716296f8
SiddikaBegum <siddikabegum29@gmail.com>
2025-03-07 10:43:52 +0530

    4th commit

--- src/org/sample/Amazon.java
+++ src/org/sample/Amazon.java
@@ -27,7 +27,7 @@ public class Amazon {
 		boolean all = p.removeAll(l);
 		System.out.println(all);
 		System.out.println(1);
-                System.out.println(2)
+                System.out.println(2);
 
 		
 		
